ed41adbe36730a9e76da68eae9ccaafd7eacc37a
shahadat10033 <shahadat10033@gmail.com>
2023-04-10 07:36:56 +0600

    make a json file named jobListion.json

---
+++ src/Components/FeaturedJob.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FeaturedJob = () => {
+  return (
+    <div className="container my-5 py-5">
+      <h3 style={{ fontSize: 48 }} className="fw-bold text-center my-3">
+        Featured Jobs
+      </h3>
+      <p className="text-center">
+        <small>
+          Explore thousands of job opportunities with all the information you
+          need. Its your future
+        </small>
+      </p>
+    </div>
+  );
+};
+
+export default FeaturedJob;
--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderArticle from "./HeaderArticle";
 import JobCategory from "./JobCategory";
+import FeaturedJob from "./FeaturedJob";
 
 const Root = () => {
   return (
     <div>
       <HeaderArticle></HeaderArticle>
       <JobCategory></JobCategory>
+      <FeaturedJob></FeaturedJob>
     </div>
   );
 };
